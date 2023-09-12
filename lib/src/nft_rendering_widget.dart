8b76a4746b20313edf16550a14c69fecbe143c58
phuoc <phuoc@bitmark.com>
2023-09-12 10:29:06 +0700

    model viewer background
    Signed-off-by: phuoc <phuoc@bitmark.com>

--- lib/src/nft_rendering_widget.dart
+++ lib/src/nft_rendering_widget.dart
@@ -1040,6 +1040,7 @@ class ModelViewerRenderingWidget extends INFTRenderingWidget {
           src: previewURL,
           ar: true,
           autoRotate: true,
+          backgroundColor: Colors.black,
         ),
       ],
     );
